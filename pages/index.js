05664489020d84e425ee9f9b08794ba4d2ff8fd6
Samuel Suh <samsuh@gmail.com>
2020-06-02 04:31:59 -0700

    styling implemented on frontend, basic routing also implemented along with a header on Layout parent component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   //SSR cannot make use of componentDidMount. NextJS uses getInitialProps to get the initial data, and sends it down to the componenet that will render HTML SSR
@@ -14,8 +16,35 @@ class CampaignIndex extends Component {
   //   const campaigns = await factory.methods.getDeployedCampaigns().call();
   //   console.log("campaigns list: ", campaigns);
   // }
+
+  renderCampaigns() {
+    //return a list of objects where each object represents a single <Card/> to render on screen
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>;
+    return (
+      <Layout>
+        <div>
+          <h3>Open Campaigns</h3>
+          <Button
+            content="Create Campaign"
+            icon="add circle"
+            primary
+            floated="right"
+          />
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
+    );
+    // {this.props.campaigns[0]}
     // return <div>Index component here</div>;
   }
 }

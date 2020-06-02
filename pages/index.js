57b6160532e5d7d952453d14b24ccc8608a637ab
Samuel Suh <samsuh@gmail.com>
2020-06-02 02:56:16 -0700

    next dev script, and changes to implement getInitialProps for SSR

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
-    // get all deployed campaigns
+  //SSR cannot make use of componentDidMount. NextJS uses getInitialProps to get the initial data, and sends it down to the componenet that will render HTML SSR
+  // 'static' keyword instantiates the class itself. do not have to create an instance.Next wants to run it without attempting to render.
+  static async getInitialprops() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log("campaigns list: ", campaigns);
+
+    return { campaigns: campaigns };
+    //now this object is sent to our component CampaignIndex as props, and can be accessed on "this.props.campaigns"
   }
+  // async componentDidMount() {
+  //   // get all deployed campaigns
+  //   const campaigns = await factory.methods.getDeployedCampaigns().call();
+  //   console.log("campaigns list: ", campaigns);
+  // }
   render() {
-    return <div>Campaigns Index Renders Here</div>;
+    return <div>{this.props.campaigns[0]}</div>;
   }
 }
 

fc95b4341cc065f6b5e4e3a6a50762e7f57c29d0
Samuel Suh <samsuh@gmail.com>
2020-06-03 07:41:48 -0700

    implement request show page, and implement Approve and Finalize buttons

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,7 +1,76 @@
 import React, { Component } from "React";
+import Layout from "../../../components/Layout";
+import { Button, Table } from "semantic-ui-react";
+import { Link } from "../../../routes";
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
+
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    // console.log(requests);
+    //returning requests will let us work with it inside the component.
+    return { address, requests, requestCount, approversCount };
+  }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          request={request}
+          address={this.props.address}
+          id={index}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
+  }
+
   render() {
-    return <h3>Request List</h3>;
+    const { Header, Row, HeaderCell, Body } = Table;
+    return (
+      <Layout>
+        <Link route={`/campaigns/${this.props.address}`}>
+          <a>Back</a>
+        </Link>
+        <h3>Requests</h3>
+        {/* Would be good to show Balance held in this Campaign here  */}
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary floated="right" style={{ marginBottom: "10px" }}>
+              Add Request
+            </Button>
+          </a>
+        </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount (ETH)</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
+        <div>Found {this.props.requestCount} Requests</div>
+      </Layout>
+    );
   }
 }
 
